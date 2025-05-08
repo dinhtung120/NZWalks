682f106f208b9c3dd9bbdd6782ffb445c4330826
Lucifer <newstarvn01@gmail.com>
2025-05-09 00:08:05 +0700

    add API GetAll Regions

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext _dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = _dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+        
+        
+        
+    }
+}
