78a5b73f4a913ca83508203e44b8d6c773825d8a
Lucifer <newstarvn01@gmail.com>
2025-05-09 23:25:40 +0700

    create CRUD Region

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers
 {
@@ -18,11 +19,85 @@ namespace NZWalks.API.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = _dbContext.Regions.ToList();
-            return Ok(regions);
+            var regionsDomains = _dbContext.Regions.ToList();
+            var regionsDto = new List<RegionDto>();
+            foreach (var regionDomain in regionsDomains)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Code=regionDomain.Code,
+                    Name=regionDomain.Name,
+                    RegionImageUrl=regionDomain.RegionImageUrl,
+                });
+            }
+            
+            return Ok(regionsDto);
         }
         
-        
-        
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var regionDomain = _dbContext.Regions.Find(id);
+            //var region = _dbContext.Regions.FirstOrDefault(x =>x.Id == id);
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+            var regionDto = new RegionDto
+            {
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl,
+            };
+           
+            return Ok(regionDto);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] RegionDto regionDto)
+        {
+            var regionDomain = new Region
+            {
+                Code = regionDto.Code,
+                Name = regionDto.Name,
+                RegionImageUrl = regionDto.RegionImageUrl,
+            };
+            _dbContext.Add(regionDomain);
+            _dbContext.SaveChanges();
+            
+            return CreatedAtAction(nameof(GetById), new { id = regionDomain.Id }, regionDto);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult Update([FromRoute] Guid id, [FromBody] RegionDto regionDto)
+        {
+            var regionDomain =  _dbContext.Regions.Find(id);
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+            regionDomain.Code = regionDto.Code;
+            regionDomain.Name = regionDto.Name;
+            regionDomain.RegionImageUrl = regionDto.RegionImageUrl;
+            _dbContext.SaveChanges();
+            
+            return Ok(regionDto);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var regionDomain = _dbContext.Regions.Find(id);
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Remove(regionDomain);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
     }
 }
---
+++ NZWalks.API/Models/DTO/RegionDto.cs
@@ -0,0 +1,8 @@
+namespace NZWalks.API.Models.DTO;
+
+public class RegionDto
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string? RegionImageUrl { get; set; }
+}
