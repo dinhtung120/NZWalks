f1aad0c89ba8121c3965e6e6884399f98eb6a1cc
Lucifer <newstarvn01@gmail.com>
2025-05-15 00:03:00 +0700

    add call Add Region

--- NZWalks.API/CustomActionFilters/ValidateModelAttribute.cs
+++ NZWalks.API/CustomActionFilters/ValidateModelAttribute.cs
@@ -7,7 +7,7 @@ public class ValidateModelAttribute:ActionFilterAttribute
 {
     public override void OnActionExecuting(ActionExecutingContext context)
     {
-        if (context.ModelState.IsValid)
+        if (!context.ModelState.IsValid)
         {
             context.Result = new BadRequestResult();
         }
--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -1,4 +1,7 @@
+using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models;
 using NZWalks.UI.Models.DTO;
 
 namespace NZWalks.UI.Controllers
@@ -13,6 +16,7 @@ namespace NZWalks.UI.Controllers
             _clientFactory = clientFactory;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             List<RegionDto> response = new List<RegionDto>();
@@ -33,6 +37,34 @@ namespace NZWalks.UI.Controllers
             }
             
             return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel model)
+        {
+            var client = _clientFactory.CreateClient();
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7159/api/Regions"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+            };
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+            
+             var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+
+             if (response is not null)
+             {
+                 return RedirectToAction("Index", "Regions");
+             }
+             return View();
         }
 
     }
